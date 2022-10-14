882dc891bda63f2089fcc820beccc0600178ba4d
esyepesv <60147085+esyepesv@users.noreply.github.com>
2022-10-14 15:57:15 -0500

    proxy

--- Proxy/Proxy.py
+++ Proxy/Proxy.py
@@ -1,6 +1,6 @@
 import socket 
 
-host , port = '127.0.0.1' , 8888
+host , port = '127.0.0.1' , 8080
 
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serversocket.setsockopt(socket.SOL_SOCKET , socket.SO_REUSEADDR , 1)
