bf38892a11bb840765422eda908bc9d7e428116e
esyepesv <60147085+esyepesv@users.noreply.github.com>
2022-10-23 21:08:06 -0500

    conexion con un servidor

--- Proxy/proxy.py
+++ Proxy/proxy.py
@@ -12,24 +12,33 @@ def listen():
     serversocket.listen(1)
     print('servidor en el puerto',port)
 
-    received = True
-    while received:
+    while True:
         connection , address = serversocket.accept()
         request = connection.recv(1024).decode('utf-8')
         print(request)
-        send(request)
-        received = False
+       
+        data_received = send(request)
+
+        print(data_received.decode(constants.ENCONDING_FORMAT))
+
+        response = str(data_received.decode(constants.ENCONDING_FORMAT))
+        connection.send(response.encode(constants.ENCONDING_FORMAT))
+
+        #serversocket.sendall(response.encode(constants.ENCONDING_FORMAT))
+
 
 def send(command_to_send):
     clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    clientsocket.connect((constants.IP_SERVER,constants.PORT2))
+    clientsocket.connect((constants.IP_SERVER2,constants.PORT))
     
     if command_to_send == '':
         print('Please input a valid command...')
     else:        
         clientsocket.send(bytes(command_to_send,constants.ENCONDING_FORMAT))
         data_received = clientsocket.recv(constants.RECV_BUFFER_SIZE)        
-        print(data_received.decode(constants.ENCONDING_FORMAT))
+        #print(data_received.decode(constants.ENCONDING_FORMAT))
+
+        return data_received
 
 if __name__ == '__main__':
     listen()
