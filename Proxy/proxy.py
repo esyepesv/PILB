69aaf30733477a676774963d8cfa8bb582160b85
esyepesv <60147085+esyepesv@users.noreply.github.com>
2022-10-25 16:44:44 -0500

    balanceador de carga

--- Proxy/proxy.py
+++ Proxy/proxy.py
@@ -3,7 +3,6 @@ import constants
 
 
 host , port =  constants.IP_SERVER, constants.PORT
-
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 def listen():
@@ -11,23 +10,25 @@ def listen():
     serversocket.bind((host , port))
     serversocket.listen(1)
     print('servidor en el puerto',port)
-
+    ip_index = 0
     while True:
         connection , address = serversocket.accept()
         request = connection.recv(1024).decode('utf-8')
         print(request)
        
-        data_received = send(request)
+        data_received = send(request, ip_index)
 
         print(data_received.decode(constants.ENCONDING_FORMAT))
 
         response = str(data_received.decode(constants.ENCONDING_FORMAT))
         connection.send(response.encode(constants.ENCONDING_FORMAT))
+        ip_index+=1 if ip_index < len(constants.IP_SERVERS)-1 else 0
+    
 
 
-def send(command_to_send):
+def send(command_to_send, ip_index):
     clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    clientsocket.connect((constants.IP_SERVERS[2],constants.PORT))
+    clientsocket.connect((constants.IP_SERVERS[ip_index],constants.PORT))
     
     if command_to_send == '':
         print('Please input a valid command...')
