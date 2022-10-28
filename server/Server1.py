fd3aa9796b22dd17a14ef82addb201615c78913b
esyepesv <60147085+esyepesv@users.noreply.github.com>
2022-10-28 11:23:05 -0500

    log

--- server/Server1.py
+++ server/Server1.py
@@ -3,69 +3,44 @@ import constants
 
 host , port = constants.IP_SERVER , constants.PORT
 
-def listen():
-    serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    serversocket.setsockopt(socket.SOL_SOCKET , socket.SO_REUSEADDR , 1)
-    serversocket.bind((host , port))
-    serversocket.listen(1)
-    print('servidor en el puerto',port)
-    return serversocket
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serversocket.setsockopt(socket.SOL_SOCKET , socket.SO_REUSEADDR , 1)
+serversocket.bind((host , port))
+serversocket.listen(1)
+print('servidor en el puerto',port)
 
+while True:
+    connection , address = serversocket.accept()
+    request = connection.recv(1024).decode('utf-8')
+    print(request)
+    string_list = request.split(' ')
+    method = string_list[0]
+    requesting_file = string_list[1]
 
-def temp(serversocket):
-    while True:
-        connection , address = serversocket.accept()
-        request = connection.recv(1024).decode('utf-8')
-        print("//////////////x1"+request+"//////////////")
-        string_list = request.split(' ')
+    print('Client request',requesting_file)
 
-        if request == "":
-            temp(serversocket)
+    myfile = requesting_file.split('?')[0]
+    myfile = myfile.lstrip('/')
 
+    if(myfile == ''):
+        myfile = 'index.html'
 
-        requesting_file = string_list[1]
+    try:
+        file = open(myfile , 'rb')
+        response = file.read()
+        file.close()
 
-        print("/////////x2"+requesting_file+"/////////")
+        header = 'HTTP/1.1 200 OK\n'
+        mimetype = 'text/html'
 
+        header += 'Content-Type: '+str(mimetype)+'\n\n'
 
-        print('Client request',requesting_file)
+    except Exception as e:
+        print("-")
+        header = 'HTTP/1.1 404 Not Found\n\n'
+        response = '<html><body>Error 404: File not found</body></html>'.encode('utf-8')
 
-        myfile = requesting_file.split('?')[0]
-        myfile = myfile.lstrip('/')
-
-        if(myfile == ''):
-            myfile = 'index.html'
-        try:
-            file = open(myfile , 'rb')
-            response = file.read()
-            file.close()
-
-            header = 'HTTP/1.1 200 OK\n'
-
-            if(myfile.endswith('.jpg')):
-                mimetype = 'image/jpg'
-            else:
-                mimetype = 'text/html'
-
-            header += 'Content-Type: '+str(mimetype)+'\n\n'
-
-            final_response = header.encode('utf-8')
-            final_response += response
-            send(connection,final_response)
-
-        except Exception as e:
-            print("-")
-            header = 'HTTP/1.1 404 Not Found\n\n'
-            response = '<html><body>Error 404: File not found</body></html>'.encode('utf-8')
-            send(connection,response)
-
-
-def send(connection,final_response):
+    final_response = header.encode('utf-8')
+    final_response += response
     connection.send(final_response)
     connection.close()
-
-
-
-if __name__ == '__main__':
-    soc = listen()
-    temp(soc)
