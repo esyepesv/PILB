88c431437b9c5600baa61c746640e44a3db1f2ec
esyepesv <60147085+esyepesv@users.noreply.github.com>
2022-10-28 10:35:03 -0500

    log

--- Proxy/proxy.py
+++ Proxy/proxy.py
@@ -1,11 +1,14 @@
 import socket 
 import constants
+import logging
 
 
 host , port =  constants.IP_SERVER, constants.PORT
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+logging.basicConfig(filename = 'Register.log', level = 'INFO')
 
-def listen():
+
+def main():
     serversocket.setsockopt(socket.SOL_SOCKET , socket.SO_REUSEADDR , 1)
     serversocket.bind((host , port))
     serversocket.listen(1)
@@ -14,21 +17,30 @@ def listen():
     while True:
         connection , address = serversocket.accept()
         request = connection.recv(1024).decode('utf-8')
+        print(address)
+
+        #log
         print(request)
+        logging.info('Consulta: %r\n recibida en el puerto: %r\n',request, port)
+
        
         data_received = send(request, ip_index)
-
-        print(data_received.decode(constants.ENCONDING_FORMAT))
-
         response = str(data_received.decode(constants.ENCONDING_FORMAT))
         connection.send(response.encode(constants.ENCONDING_FORMAT))
+
+        #log
+        print(response)
+        logging.info('respuesta: %r\n enviada por el puerto: %r\n', response, port)
+
+        #cambio de server
         ip_index+=1 if ip_index < len(constants.IP_SERVERS)-1 else 0
+
     
 
 
 def send(command_to_send, ip_index):
     clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    clientsocket.connect((constants.IP_SERVERS[ip_index],constants.PORT))
+    clientsocket.connect((constants.IP_SERVER,constants.PORT))
     
     if command_to_send == '':
         print('Please input a valid command...')
@@ -39,5 +51,6 @@ def send(command_to_send, ip_index):
 
         return data_received
 
+
 if __name__ == '__main__':
-    listen()
+    main()
--- server/Server1.py
+++ server/Server1.py
@@ -3,48 +3,69 @@ import constants
 
 host , port = constants.IP_SERVER , constants.PORT
 
-serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-serversocket.setsockopt(socket.SOL_SOCKET , socket.SO_REUSEADDR , 1)
-serversocket.bind((host , port))
-serversocket.listen(1)
-print('servidor en el puerto',port)
+def listen():
+    serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    serversocket.setsockopt(socket.SOL_SOCKET , socket.SO_REUSEADDR , 1)
+    serversocket.bind((host , port))
+    serversocket.listen(1)
+    print('servidor en el puerto',port)
+    return serversocket
 
-while True:
-    connection , address = serversocket.accept()
-    request = connection.recv(1024).decode('utf-8')
-    print(request)
-    string_list = request.split(' ')
-    method = string_list[0]
-    requesting_file = string_list[1]
 
-    print('Client request',requesting_file)
+def temp(serversocket):
+    while True:
+        connection , address = serversocket.accept()
+        request = connection.recv(1024).decode('utf-8')
+        print("//////////////x1"+request+"//////////////")
+        string_list = request.split(' ')
 
-    myfile = requesting_file.split('?')[0]
-    myfile = myfile.lstrip('/')
+        if request == "":
+            temp(serversocket)
 
-    if(myfile == ''):
-        myfile = 'index.html'
 
-    try:
-        file = open(myfile , 'rb')
-        response = file.read()
-        file.close()
+        requesting_file = string_list[1]
 
-        header = 'HTTP/1.1 200 OK\n'
+        print("/////////x2"+requesting_file+"/////////")
 
-        if(myfile.endswith('.jpg')):
-            mimetype = 'image/jpg'
-        else:
-            mimetype = 'text/html'
 
-        header += 'Content-Type: '+str(mimetype)+'\n\n'
+        print('Client request',requesting_file)
 
-    except Exception as e:
-        print("-")
-        header = 'HTTP/1.1 404 Not Found\n\n'
-        response = '<html><body>Error 404: File not found</body></html>'.encode('utf-8')
+        myfile = requesting_file.split('?')[0]
+        myfile = myfile.lstrip('/')
 
-    final_response = header.encode('utf-8')
-    final_response += response
+        if(myfile == ''):
+            myfile = 'index.html'
+        try:
+            file = open(myfile , 'rb')
+            response = file.read()
+            file.close()
+
+            header = 'HTTP/1.1 200 OK\n'
+
+            if(myfile.endswith('.jpg')):
+                mimetype = 'image/jpg'
+            else:
+                mimetype = 'text/html'
+
+            header += 'Content-Type: '+str(mimetype)+'\n\n'
+
+            final_response = header.encode('utf-8')
+            final_response += response
+            send(connection,final_response)
+
+        except Exception as e:
+            print("-")
+            header = 'HTTP/1.1 404 Not Found\n\n'
+            response = '<html><body>Error 404: File not found</body></html>'.encode('utf-8')
+            send(connection,response)
+
+
+def send(connection,final_response):
     connection.send(final_response)
     connection.close()
+
+
+
+if __name__ == '__main__':
+    soc = listen()
+    temp(soc)
